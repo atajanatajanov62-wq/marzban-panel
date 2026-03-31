763fdc3c0f74f4e2397dbe2a3168fae3b0a25219
atajanatajanov62-wq <atajanatajanov62@gmail.com>
2026-03-31 08:16:24 +0430

    feat: Professional X Panel — src/utils/formatByte.tsx

---
+++ src/utils/formatByte.tsx
@@ -0,0 +1,17 @@
+export function formatBytes(bytes: number, decimals = 2, asArray = false) {
+  if (bytes == null || !+bytes) return asArray ? ["0", "B"] : "0 B";
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  if (!asArray)
+    return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
+  else return [parseFloat((bytes / Math.pow(k, i)).toFixed(dm)), sizes[i]];
+}
+
+export const numberWithCommas = (x: number) => {
+  if (x == null) return "0";
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
